f8bbff9c25c565cb3354190a2616ca05a1c9b7ec
EclipseSpark <eclipse-github@frozenbox.org>
2016-02-22 15:27:49 +0100

    Update parrot-install.sh

--- parrot-install.sh
+++ parrot-install.sh
@@ -30,14 +30,16 @@ function option_picked() {
 
 
 function core_install() {
-	echo -e "deb http://archive.parrotsec.org/mirrors/parrot stable main contrib non-free\ndeb http://archive.parrotsec.org/mirrors/parrot stable-security main contrib non-free" > /etc/apt/sources.list.d/parrot.list
-	#echo -e "deb http://eu.archive.parrotsec.org/mirrors/debian jessie main contrib non-free\n#deb-src http://eu.archive.parrotsec.org/mirrors/debian jessie main contrib non-free\n\n#deb http://eu.archive.parrotsec.org/mirrors/debian jessie-backports main contrib non-free\n#deb-src http://eu.archive.parrotsec.org/mirrors/debian jessie-backports main contrib non-free\n\ndeb http://security.debian.org/ jessie/updates main\n#deb-src http://security.debian.org/ jessie/updates main\n\ndeb http://eu.archive.parrotsec.org/mirrors/debian jessie-updates main contrib non-free\n#deb-src http://eu.archive.parrotsec.org/mirrors/debian jessie-updates main contrib non-free\n\n#deb http://eu.archive.parrotsec.org/mirrors/debian jessie-proposed-updates main contrib non-free\n#deb-src http://eu.archive.parrotsec.org/mirrors/debian jessie-updates main contrib non-free" > /etc/apt/sources.list.d/debian.list
+	echo -e "deb http://euro3.archive.parrotsec.org/parrotsec stable main contrib non-free\ndeb http://archive.parrotsec.org/parrot stable-security main contrib non-free" > /etc/apt/sources.list.d/parrot.list
+	echo -e "# This file is empty, feel free to add here your custom APT repositories\n\n# The standard Parrot repositories are NOT here. If you want to\n# edit them, take a look into\n#                      /etc/apt/sources.list.d/parrot.list\n#                      /etc/apt/sources.list.d/debian.list\n\n\n\n# If you want to change the default parrot repositories setting\n# another localized mirror, then use the command parrot-mirror-selector\n# and see its usage message to know what mirrors are available\n\n\n\n#uncomment the following line to enable the Parrot Testing Repository\n#deb http://us.repository.frozenbox.org/parrot testing main contrib nonfree" > /etc/apt/sources.list
 	wget -qO - http://archive.parrotsec.org/parrot/misc/parrotsec.gpg | apt-key add -
 	apt-get update
 	apt-get -y --force-yes install apt-parrot parrot-archive-keyring --no-install-recommends
+	parrot-mirror-selector euro3 #change it if you want another mirror, launch it without parameters to get the full list of available mirrors
 	apt-get update
 	apt-get -y --force-yes install parrot-core
 	apt-get -y --force-yes dist-upgrade
+	apt-get -y --force-yes autoremove
 }
 
 function cloud_install() {
