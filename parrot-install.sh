10ddbcd4947780772677e8ae88b589bda3643066
Lorenzo Faletra <eclipse-github@frozenbox.org>
2016-05-17 22:02:59 +0200

    Update parrot-install.sh

--- parrot-install.sh
+++ parrot-install.sh
@@ -30,7 +30,7 @@ function option_picked() {
 
 
 function core_install() {
-	echo -e "deb http://euro3.archive.parrotsec.org/parrotsec stable main contrib non-free\ndeb http://archive.parrotsec.org/parrot stable-security main contrib non-free" > /etc/apt/sources.list.d/parrot.list
+	echo -e "deb http://euro3.archive.parrotsec.org/parrotsec parrot main contrib non-free" > /etc/apt/sources.list.d/parrot.list
 	echo -e "# This file is empty, feel free to add here your custom APT repositories\n\n# The standard Parrot repositories are NOT here. If you want to\n# edit them, take a look into\n#                      /etc/apt/sources.list.d/parrot.list\n#                      /etc/apt/sources.list.d/debian.list\n\n\n\n# If you want to change the default parrot repositories setting\n# another localized mirror, then use the command parrot-mirror-selector\n# and see its usage message to know what mirrors are available\n\n\n\n#uncomment the following line to enable the Parrot Testing Repository\n#deb http://us.repository.frozenbox.org/parrot testing main contrib nonfree" > /etc/apt/sources.list
 	wget -qO - http://archive.parrotsec.org/parrot/misc/parrotsec.gpg | apt-key add -
 	apt-get update
