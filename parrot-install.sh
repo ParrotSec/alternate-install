8a67c95aa8415fed1e75b31737cc450bd2557f25
Lorenzo "Palinuro" Faletra <eclipse-github@frozenbox.org>
2016-11-20 21:50:29 +0100

    update repository layout

--- parrot-install.sh
+++ parrot-install.sh
@@ -30,12 +30,12 @@ function option_picked() {
 
 
 function core_install() {
-	echo -e "deb http://euro3.archive.parrotsec.org/parrotsec parrot main contrib non-free" > /etc/apt/sources.list.d/parrot.list
+	echo -e "deb http://cloudflare.archive.parrotsec.org/parrot parrot main contrib non-free" > /etc/apt/sources.list.d/parrot.list
 	echo -e "# This file is empty, feel free to add here your custom APT repositories\n\n# The standard Parrot repositories are NOT here. If you want to\n# edit them, take a look into\n#                      /etc/apt/sources.list.d/parrot.list\n#                      /etc/apt/sources.list.d/debian.list\n\n\n\n# If you want to change the default parrot repositories setting\n# another localized mirror, then use the command parrot-mirror-selector\n# and see its usage message to know what mirrors are available\n\n\n\n#uncomment the following line to enable the Parrot Testing Repository\n#deb http://us.repository.frozenbox.org/parrot testing main contrib nonfree" > /etc/apt/sources.list
 	wget -qO - http://archive.parrotsec.org/parrot/misc/parrotsec.gpg | apt-key add -
 	apt-get update
 	apt-get -y --force-yes install apt-parrot parrot-archive-keyring --no-install-recommends
-	parrot-mirror-selector euro3 #change it if you want another mirror, launch it without parameters to get the full list of available mirrors
+	parrot-mirror-selector 8 stable #change it if you want another mirror, launch it without parameters to get the full list of available mirrors
 	apt-get update
 	apt-get -y --force-yes install parrot-core
 	apt-get -y --force-yes dist-upgrade
