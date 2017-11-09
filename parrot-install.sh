5965874d16ce13da8f21dfd0291f1fa574de5b4d
owen <owen@DTC20851bd5957D>
2017-11-09 09:05:24 -0500

    Fixed the improper characters that prevented this script from running. R U Drunk?

--- parrot-install.sh
+++ parrot-install.sh
@@ -34,32 +34,32 @@ function core_install() {
 	echo -e "# This file is empty, feel free to add here your custom APT repositories\n\n# The standard Parrot repositories are NOT here. If you want to\n# edit them, take a look into\n#                      /etc/apt/sources.list.d/parrot.list\n#                      /etc/apt/sources.list.d/debian.list\n\n\n\n# If you want to change the default parrot repositories setting\n# another localized mirror, then use the command parrot-mirror-selector\n# and see its usage message to know what mirrors are available\n\n\n\n#uncomment the following line to enable the Parrot Testing Repository\n#deb http://us.repository.frozenbox.org/parrot testing main contrib nonfree" > /etc/apt/sources.list
 	wget -qO - https://archive.parrotsec.org/parrot/misc/parrotsec.gpg | apt-key add -
 	apt-get update
-	apt-get -y --force-yes -o Dpkg::Options::=”–force-overwrite” install apt-parrot parrot-archive-keyring --no-install-recommends
+	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install apt-parrot parrot-archive-keyring --no-install-recommends
 	parrot-mirror-selector default stable #change it if you want another mirror, launch it without parameters to get the full list of available mirrors
 	apt-get update
-	apt-get -y --force-yes -o Dpkg::Options::=”–force-overwrite” install parrot-core
-	apt-get -y --force-yes -o Dpkg::Options::=”–force-overwrite” dist-upgrade
-	apt-get -y --force-yes -o Dpkg::Options::=”–force-overwrite” autoremove
+	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install parrot-core
+	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" dist-upgrade
+	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" autoremove
 }
 
 function headless_install() {
-	apt-get -y --force-yes -o Dpkg::Options::=”–force-overwrite” install  parrot-pico
+	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install  parrot-pico
 }
 
 function standard_install() {
-	apt-get -y --force-yes -o Dpkg::Options::=”–force-overwrite” install parrot-interface parrot-tools
+	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install parrot-interface parrot-tools
 }
 
 function full_install() {
-	apt-get -y --force-yes -o Dpkg::Options::=”–force-overwrite” install parrot-interface parrot-interface-full parrot-tools-full
+	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install parrot-interface parrot-interface-full parrot-tools-full
 }
 
 function home_install() {
-	apt-get -y --force-yes -o Dpkg::Options::=”–force-overwrite” install parrot-interface-full parrot-interface
+	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install parrot-interface-full parrot-interface
 }
 
 function embedded_install() {
-	apt-get -y --force-yes -o Dpkg::Options::=”–force-overwrite” install parrot-interface parrot-mini
+	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install parrot-interface parrot-mini
 }
 
 
