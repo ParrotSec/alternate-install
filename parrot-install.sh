29cdc0fae79cc47b3a42e85efcf1a78b5d16e80a
Lorenzo "Palinuro" Faletra <palinuro@parrotsec.org>
2020-02-17 17:07:46 +0100

    make it work again

--- parrot-install.sh
+++ parrot-install.sh
@@ -12,10 +12,9 @@ show_menu(){
     echo -e "\t\trev 0.2 - 2015-06-10"
     echo -e "${MENU}**${NUMBER} 1)${MENU} Install Core Only ${NORMAL}"
     echo -e "${MENU}**${NUMBER} 2)${MENU} Install Headless Edition ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 3)${MENU} Install Standard Edition ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 4)${MENU} Install Full Edition ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 5)${MENU} Install Home Edition ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 6)${MENU} Install Embedded Edition ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 3)${MENU} Install Security Edition ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 4)${MENU} Install Home Edition ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 5)${MENU} Install Embedded Edition ${NORMAL}"
     echo -e "${MENU}*********************************************${NORMAL}"
     echo -e "${ENTER_LINE}Please enter a menu option and enter or ${RED_TEXT}enter to exit. ${NORMAL}"
     read opt
@@ -32,37 +31,32 @@ function option_picked() {
 function core_install() {
 	# Protect against HTTP vulnerabilities [https://www.debian.org/security/2016/dsa-3733], [https://www.debian.org/security/2019/dsa-4371]
 	apt-get update
-	apt-get -y install apt-transport-https
-	echo -e "deb https://mirrordirector.archive.parrotsec.org/parrot parrot main contrib non-free" > /etc/apt/sources.list.d/parrot.list
+	echo -e "deb https://mirror.parrotsec.org/parrot rolling main contrib non-free" > /etc/apt/sources.list.d/parrot.list
 	echo -e "# This file is empty, feel free to add here your custom APT repositories\n\n# The standard Parrot repositories are NOT here. If you want to\n# edit them, take a look into\n#                      /etc/apt/sources.list.d/parrot.list\n#                      /etc/apt/sources.list.d/debian.list\n\n\n\n# If you want to change the default parrot repositories setting\n# another localized mirror, then use the command parrot-mirror-selector\n# and see its usage message to know what mirrors are available\n\n\n\n#uncomment the following line to enable the Parrot Testing Repository\n#deb http://us.repository.frozenbox.org/parrot testing main contrib nonfree" > /etc/apt/sources.list
-	wget -qO - https://archive.parrotsec.org/parrot/misc/parrotsec.gpg | apt-key add -
+	wget -qO - https://deb.parrotsec.org/parrot/misc/parrotsec.gpg | apt-key add -
 	apt-get update
 	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install apt-parrot parrot-archive-keyring --no-install-recommends
 	parrot-mirror-selector default stable #change it if you want another mirror, launch it without parameters to get the full list of available mirrors
 	apt-get update
-	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install parrot-core
-	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" dist-upgrade
-	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" autoremove
+	apt -y --allow-downgrades -o Dpkg::Options::="--force-overwrite" install parrot-core
+	apt -y --allow-downgrades -o Dpkg::Options::="--force-overwrite" dist-upgrade
+	apt -y autoremove
 }
 
 function headless_install() {
-	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install  parrot-pico
+	apt -y --allow-downgrades install parrot-pico
 }
 
-function standard_install() {
-	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install parrot-interface parrot-tools
-}
-
-function full_install() {
-	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install parrot-interface parrot-interface-full parrot-tools-full
+function security_install() {
+	apt -y --allow-downgrades install parrot-interface parrot-interface-full parrot-tools-full
 }
 
 function home_install() {
-	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install parrot-interface-full parrot-interface
+	apt -y --allow-downgrades install parrot-interface-full parrot-interface
 }
 
 function embedded_install() {
-	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install parrot-interface parrot-mini
+	apt -y --allow-downgrades install parrot-interface parrot-mini
 }
 
 
@@ -94,26 +88,19 @@ while [ opt != '' ]
         3) clear;
 		option_picked "Installing Parrot Security OS";
 		core_install;
-		standard_install;
+		security_install;
 		option_picked "Operation Done!";
 		exit;
             ;;
 
-        4) clear;
-		option_picked "Installing Full Edition";
-		core_install;
-		full_install;
-		option_picked "Operation Done!";
-		exit;
-            ;;
-	5) clear;
+	4) clear;
 		option_picked "Installing Home Edition";
 		core_install;
 		home_install;
 		option_picked "Operation Done!";
 		exit;
 		;;
-	6) clear;
+	5) clear;
 		option_picked "Installing Embedded Edition";
 		core_install;
 		embedded_install;
