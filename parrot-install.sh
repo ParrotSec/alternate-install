41424c32db68b13048f9491d8a6322d58e4b40d2
Lorenzo "Palinuro" Faletra <palinuro@parrotsec.org>
2020-03-19 11:57:39 +0000

    Update parrot-install.sh

--- parrot-install.sh
+++ parrot-install.sh
@@ -31,8 +31,10 @@ function option_picked() {
 function core_install() {
 	# Protect against HTTP vulnerabilities [https://www.debian.org/security/2016/dsa-3733], [https://www.debian.org/security/2019/dsa-4371]
 	apt-get update
-	echo -e "deb https://mirror.parrotsec.org/parrot rolling main contrib non-free" > /etc/apt/sources.list.d/parrot.list
-	echo -e "# This file is empty, feel free to add here your custom APT repositories\n\n# The standard Parrot repositories are NOT here. If you want to\n# edit them, take a look into\n#                      /etc/apt/sources.list.d/parrot.list\n#                      /etc/apt/sources.list.d/debian.list\n\n\n\n# If you want to change the default parrot repositories setting\n# another localized mirror, then use the command parrot-mirror-selector\n# and see its usage message to know what mirrors are available\n\n\n\n#uncomment the following line to enable the Parrot Testing Repository\n#deb http://us.repository.frozenbox.org/parrot testing main contrib nonfree" > /etc/apt/sources.list
+	apt-get -y full-upgrade
+	apt-get -y install gnupg
+	echo -e "deb https://mirror.parrot.sh/parrot rolling main contrib non-free" > /etc/apt/sources.list.d/parrot.list
+	echo -e "# The parrot repo is located at /etc/apt/sources.list.d/parrot.list" > /etc/apt/sources.list
 	wget -qO - https://deb.parrotsec.org/parrot/misc/parrotsec.gpg | apt-key add -
 	apt-get update
 	apt-get -y --force-yes -o Dpkg::Options::="--force-overwrite" install apt-parrot parrot-archive-keyring --no-install-recommends
